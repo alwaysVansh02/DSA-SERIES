544f8cd192644b6a1c164a26f088a4845da2c4e7
Vansh  S <vanshsinghrajput746@gmail.com>
2025-10-09 21:55:49 +0530

    Update 04_GFGRotateArray.java

--- Arrays/04_GFGRotateArray.java
+++ Arrays/04_GFGRotateArray.java
@@ -28,7 +28,7 @@ public class GFGRotateArray {
                 arr[n - d - i] = temp[i];
 
             }
-        }
+    }.  
 
     public static void main(String[] args) {
         int []arr = {1,2,3,4,5,6};
