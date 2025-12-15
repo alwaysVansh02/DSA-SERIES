16428a80391a9a90a48963395310a70c82b2dc8e
Vansh  S <vanshsinghrajput746@gmail.com>
2025-12-15 23:26:33 +0530

    Create Anagram_String_Check.java

---
+++ STRINGS/Anagram_String_Check.java
@@ -0,0 +1,20 @@
+package BinarySearch_Problem;
+import java.util.Arrays;
+class solution {
+    public static boolean areAnagrams(String sl, String s2) {
+
+        if (sl.length() != s2.length()) {
+            return false;
+        } else {
+            char chl[] = sl.toCharArray();
+            char ch2[] = s2.toCharArray();
+            Arrays.sort(chl);
+            Arrays.sort(ch2);
+            if (Arrays.equals(chl, ch2)) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+    }
+}
