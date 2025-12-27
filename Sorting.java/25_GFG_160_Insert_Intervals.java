8afaaf9cf7e484c4edaaa12f5046b3462bd3155f
Vansh  S <vanshsinghrajput746@gmail.com>
2025-12-27 18:10:25 +0530

    Fix interval merging logic in Insert Intervals

--- Sorting.java/25_GFG_160_Insert_Intervals.java
+++ Sorting.java/25_GFG_160_Insert_Intervals.java
@@ -14,7 +14,7 @@ public class Insert_Interval {
             }
             while (i < n && intervals[i][0] <= newInterval[1]) {
                 newInterval[0] = Math.min(newInterval[0], intervals[i][0]);
-                newInterval[1] = Math.max(newInterval[1], intervals[i][1]);
+                newInterval[1] = Math.max(newInterval[1], intervals[i][1]); 
                 i++;
             }
             result.add(newInterval);
