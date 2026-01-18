d5fb377e000c6bb46eb11f8c2de6490b5a604b19
Vansh  S <vanshsinghrajput746@gmail.com>
2026-01-18 19:47:44 +0530

    Create 38_GFG_160_Find_Target_InSorted_RowAndColoumn.java

---
+++ MATRIX/38_GFG_160_Find_Target_InSorted_RowAndColoumn.java
@@ -0,0 +1,24 @@
+package Matrices;
+import java.util.*;
+public class Search_In_RowAndColumn_Wise_TwoD_Array {
+    static boolean matsearch(int[][]mat,int x){
+        int n = mat.length, m = mat[0].length;
+        int i=0, j = m - 1;
+        while (i < n && j>=0){
+            if (x > mat[i][j]){
+                i++;
+            } else if (x < mat[i][j]) {
+                j--;
+            }else
+                return true;
+        }
+        return false;
+    }
+    public static void main(String[] args) {
+        int [][] mat = {{3,30,38},
+                        {20,52,54},
+                        {35,60,69}};
+        int x = 35;
+        System.out.println(matsearch(mat, x));
+    }
+}
