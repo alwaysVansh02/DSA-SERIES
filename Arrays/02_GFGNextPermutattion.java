4ac739ee6df53f9ecb53a2201e4fb5cf733ef0b4
Vansh  S <vanshsinghrajput746@gmail.com>
2026-01-25 23:05:22 +0530

    Fix formatting issue in swap method

--- Arrays/02_GFGNextPermutattion.java
+++ Arrays/02_GFGNextPermutattion.java
@@ -35,7 +35,7 @@ public class GFGNextPermutattion {
    public void swap(int[] arr, int i, int j){
         int temp = arr[i];
         arr[i] = arr[j];
-        arr[j] = temp;
+        arr[j] = temp; 
    }
    public void reverse(int [] arr, int i , int j  ){
         while(i<j){
