599d712c813d335cb5037606be311a12ce6a1a3f
Vansh  S <vanshsinghrajput746@gmail.com>
2026-03-03 15:49:44 +0530

    Create 37_GFG_160_Two_Sum_WithGiven_Sum.java

---
+++ HASHSET/37_GFG_160_Two_Sum_WithGiven_Sum.java
@@ -0,0 +1,30 @@
+package Hashing;
+    import java.util.Arrays;
+    import java.util.Map;
+import java.util.HashMap;
+
+public class TwoSum_Count_Points_WithGiven_Sum {
+   static int countPairs(int[] arr, int target) {
+            Map<Integer, Integer> freq = new HashMap<>();
+            int cnt = 0;
+
+            for (int i = 0; i < arr.length; i++) {
+
+                if (freq.containsKey(target - arr[i])) {
+                    cnt += freq.get(target - arr[i]);
+                }
+                freq.put(arr[i],
+                        freq.getOrDefault(arr[i], 0) + 1);
+            }
+            return cnt;
+        }
+
+        public static void main(String[] args) {
+            int[] arr = {1, 5, 7, -1, 5};
+            int target = 6;
+            System.out.println(countPairs(arr, target));
+        }
+
+    }
+
+
