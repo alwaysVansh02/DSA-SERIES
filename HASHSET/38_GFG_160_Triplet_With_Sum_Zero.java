0e54b323bdadaa88fc7edd88b113d22399cc7db4
Vansh  S <vanshsinghrajput746@gmail.com>
2026-03-05 09:01:09 +0530

    Create 38_GFG_160_Triplet_With_Sum_Zero.java

---
+++ HASHSET/38_GFG_160_Triplet_With_Sum_Zero.java
@@ -0,0 +1,35 @@
+package Hashing;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+
+public class Three_Sum_FindTriplet_WithSum_Zero {
+   static List<List<Integer>> findTriplet (int[] arr){
+        HashMap<Integer, List<Integer>> map = new HashMap<>();
+        List<List<Integer>> ans = new ArrayList<>();
+        for (int j =0;j<arr.length;j++){
+            for (int k = j + 1;k< arr.length;k++){
+                int val =  -1 *(arr[j] + arr[k]);
+                if (map.containsKey(val)){
+                    for (int i : map.get(val)){
+                        ans.add(Arrays.asList(i,j,k));
+                    }
+                }
+            }
+            map.putIfAbsent(arr[j], new ArrayList<>());
+            map.get(arr[j]).add(j);
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {0,-1,2,-3,1};
+        List<List<Integer>> result = findTriplet(arr);
+        for (List<Integer> triplet : result){
+            System.out.println(triplet.get(0) + " " + triplet.get(1) + " " + triplet.get(2));
+
+        }
+    }
+}
