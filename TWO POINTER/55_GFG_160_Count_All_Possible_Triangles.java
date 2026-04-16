90ac4218f0944c44d8c82e59756a7bba680f4c8f
Vansh  S <vanshsinghrajput746@gmail.com>
2026-04-16 22:47:17 +0530

    Create 55_GFG_160_Count_All_Possible_Triangles.java

---
+++ TWO POINTER/55_GFG_160_Count_All_Possible_Triangles.java
@@ -0,0 +1,27 @@
+package DSATWOPointerTechnique;
+
+import java.util.Arrays;
+
+public class Count_Pssible_Triangle {
+      static int countTriangles(int[] arr){
+        int res =0;
+        Arrays.sort(arr);
+        for (int i = 2; i<arr.length; ++i){
+            int left =0 , right  = i - 1;
+            while (left < right){
+                if (arr[left] + arr[right] > arr[i]){
+                    res += right - left;
+                    right--;
+                }else
+                    left ++;
+            }
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {4,6,3,7};
+        System.out.println("Count Of Possible Traingle: " + countTriangles(arr));
+    }
+}
+
