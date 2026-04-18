802c890ff6c233db368772122d3bb2447e6bd23b
Vansh  S <vanshsinghrajput746@gmail.com>
2026-04-18 23:44:27 +0530

    Update 55_GFG_160_Count_All_Possible_Triangles.java

--- TWO POINTER/55_GFG_160_Count_All_Possible_Triangles.java
+++ TWO POINTER/55_GFG_160_Count_All_Possible_Triangles.java
@@ -6,7 +6,7 @@ public class Count_Pssible_Triangle {
       static int countTriangles(int[] arr){
         int res =0;
         Arrays.sort(arr); 
-        for (int i = 2; i<arr.length; ++i){
+        for (int i = 2; i<arr.length; ++i){ 
             int left =0 , right  = i - 1;
             while (left < right){
                 if (arr[left] + arr[right] > arr[i]){
