167bc1b1b57ddd549d356c36d78b77caf1ba72a7
Vansh  S <vanshsinghrajput746@gmail.com>
2026-06-06 22:36:34 +0530

    Update 63_GFG_160_Longest_Subarray_With_Equal_zeroandones.java

--- PREFIX SUM/63_GFG_160_Longest_Subarray_With_Equal_zeroandones.java
+++ PREFIX SUM/63_GFG_160_Longest_Subarray_With_Equal_zeroandones.java
@@ -16,7 +16,7 @@ public class Longest_Subarray_EqualNo_ZeroOne {
             int prefsum = 0;
             int res =0;
             for (int i = 0; i< arr.length;i++){
-                prefsum += (arr[i] == 0) ? -1 : 1;
+                prefsum += (arr[i] == 0) ? -1 : 1; 
                 if (prefsum == 0)
                     res = i + 1;
                 if (mp . containsKey(prefsum))
